423bc560e2eab0e6173b47ea807e29a087a36071
Jackson Paris <38115407+parisj02@users.noreply.github.com>
2022-07-26 15:36:09 -0700

    Update streamlit_app.py
    changed ordering

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
+# Display the tastreamlit.dataframe(my_fruit_list)ble on the page.
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -14,4 +14,4 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
+
